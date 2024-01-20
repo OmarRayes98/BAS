1de554d1de5c666c8c249621a2d0bfc80c37dcaf
abdullatif <abdullatifjr002@gmail.com>
2024-01-20 17:49:08 +0300

    updated footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,22 @@ import heroImage from './../../assets/img/Group.svg'
 import LatestProjects from '../../components/Home/SpecialComponentFor_Home_page/LatestProjects/LatestProjects'
 import Partners from '../../components/Home/SpecialComponentFor_Home_page/Partners/Partners'
 import FocalImg from './../../assets/img/focalx.png'
+// import WhatWeDo from '../../components/Home/SpecialComponentFor_Home_page/WhatWeDo/WhatWeDo'
+// import WhoWeAre from '../../components/Home/SpecialComponentFor_Home_page/WhoWeAre/WhoWeAre'
+// import Reviews from '../../components/Home/SpecialComponentFor_Home_page/Reviews/Reviews'
+// import FAQs from '../../components/Home/SpecialComponentFor_Home_page/FAQs/FAQs'
 
 const Home = () => {
 
     return (
         <div className='home'>
             <Hero title="Black Analysis Solution" text="Leave it to us" heroImage={heroImage}/>
+            {/* <WhatWeDo />
+            <WhoWeAre /> */}
             <LatestProjects img={FocalImg}/>
+            {/* <Reviews /> */}
             <Partners />
+            {/* <FAQs /> */}
         </div>
     )
 
